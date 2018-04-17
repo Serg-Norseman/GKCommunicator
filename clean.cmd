4ab32aad084c50ed4cd7e3f7d16882d4b241fb11
Sergey V. Zhdanovskih <serg.zhdanovskih@yandex.ru>
2018-04-17 19:43:47 +0500

    Completion of the merge

--- clean.cmd
+++ clean.cmd
@@ -8,3 +8,9 @@ rmdir .\GKNetPNRP\obj /s /q
 
 rmdir .\GKSimpleChat\bin /s /q
 rmdir .\GKSimpleChat\obj /s /q
+
+rmdir .\DHT\bin /s /q
+rmdir .\DHT\obj /s /q
+
+rmdir .\DHTSample\bin /s /q
+rmdir .\DHTSample\obj /s /q
